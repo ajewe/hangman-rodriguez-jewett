0f9c38da4c6ee8aa1c0438cdd284b4b3e2078d36
rodrieri000 <ericarodriguez7@outlook.com>
2019-12-05 20:42:59 -0600

    Updates

--- hangman.js
+++ hangman.js
@@ -1,1 +1,21 @@
-yo
+// create hangman word
+let arrayAnswer = ['h', 'e', 'l', 'l', 'o']
+// create array of dashes
+let arrayGrid = ['-', '-', '-', '-', '-']
+// user inputs one letter 
+// add letter to grid array in coorisponding spot
+function checkInput(input) {
+  for (let i = 0; i < arrayAnswer.length; i++) {
+    if (input === arrayAnswer[i]){
+    // check against correct word
+    arrayGrid[i] = arrayAnswer[i]
+    // if array has letter, push to correct dash
+    } else wrongGuess[input];
+  }
+}
+function wrongGuess(input) {
+  // add function for incorrect input
+  // if user is incorrect, add body parts 
+
+}
+// have box for previous guesses

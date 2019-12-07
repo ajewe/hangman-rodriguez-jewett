e57a03be9ea0fc1b97ae4680abaa5f0c239377a6
ajewe <ajewe@utexas.edu>
2019-12-07 15:51:13 -0600

    array connected to dashes in html

--- hangman.js
+++ hangman.js
@@ -2,23 +2,27 @@
 let arrayAnswer = ['h', 'e', 'l', 'l', 'o']
 
 //need array with dashes
-let arrayGrid = ['-', '-', '-', '-', '-']
+// let arrayGrid = ['-', '-', '-', '-', '-']
 
 //user inputs 1 letter, check that agains 'arrayAnswer' 
-function checkInput(input) {
+function checkInput() {
+
+  let input = document.getElementById('input').value
+console.log('click click click', input)
+
   for (let i = 0; i < arrayAnswer.length; i++) {
     if (input === arrayAnswer[i]){
       // add that letter to the grid array at that corresponding spot
-      arrayGrid[i] = arrayAnswer[i]
+      document.getElementById('array').children[i].innerHTML = arrayAnswer[i]
       
-    } else wrongGuess(input);
+    }
+    // else wrongGuess(input);
   }
 };
 
 //make a wrong function and run it in the else of checkInput where it will 
 //add the guess to a guess bank
 
-function wrongGuess(input) {
+// function wrongGuess(input) {
 
   // add input to guess bank
-}

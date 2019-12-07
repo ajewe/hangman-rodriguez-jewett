6499a64a1ea91378aa2a5b6fc377bbde22ce6308
rodrieri000 <ericarodriguez7@outlook.com>
2019-12-07 14:09:43 -0600

    Updates

--- hangman.js
+++ hangman.js
@@ -1,21 +1,24 @@
-// create hangman word
+//need a word (is answer) 'arrayAnswer', stays hidden
 let arrayAnswer = ['h', 'e', 'l', 'l', 'o']
-// create array of dashes
+
+//need array with dashes
 let arrayGrid = ['-', '-', '-', '-', '-']
-// user inputs one letter 
-// add letter to grid array in coorisponding spot
+
+//user inputs 1 letter, check that agains 'arrayAnswer' 
 function checkInput(input) {
   for (let i = 0; i < arrayAnswer.length; i++) {
     if (input === arrayAnswer[i]){
-    // check against correct word
-    arrayGrid[i] = arrayAnswer[i]
-    // if array has letter, push to correct dash
-    } else wrongGuess[input];
+      // add that letter to the grid array at that corresponding spot
+      arrayGrid[i] = arrayAnswer[i]
+      
+    } else wrongGuess(input);
   }
-}
-function wrongGuess(input) {
-  // add function for incorrect input
-  // if user is incorrect, add body parts 
+};
 
+//make a wrong function and run it in the else of checkInput where it will 
+//add the guess to a guess bank
+
+function wrongGuess(input) {
+
+  // add input to guess bank
 }
-// have box for previous guesses
